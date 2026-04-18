904052dc6763cf5b1452bf5a84bdc33d4aff0887
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-04-18 17:46:31 -0400

    Capitalized fields for naming convention.

--- backend/src/SmartHome.Domain/DeviceModels.cs
+++ backend/src/SmartHome.Domain/DeviceModels.cs
@@ -1,24 +1,24 @@
 // basic template of what all devices will need in the home simulator
 public abstract class Device : IDevice
 {   //core fields and properties accounted for first
-    public Guid id {get; protected set;}
-    public string deviceName {get; protected set;}
-    public string deviceLocation {get; protected set;}
-    public DeviceType type {get; protected set;}
+    public Guid Id {get; protected set;} = Guid.NewGuid();
+    public string DeviceName {get; protected set;}
+    public string DeviceLocation {get; protected set;}
+    public DeviceType Type {get; protected set;}
 
 //each device needs to be able to control/manage its own state
-    public abstract DeviceState state {get;}
+    public abstract DeviceState State {get;}
     public abstract bool IsDeviceOn {get;}
 
 //useful for logging and auditing, also mentioned within section 2
-    public Dateime createdAt {get; protected set;} = Dateime.UtcNow;
-    public Dateime updatedAt {get; protected set;} = Dateime.UtcNow;
+    public DateTime CreatedAt {get; protected set;} = DateTime.UtcNow;
+    public DateTime UpdatedAt {get; protected set;} = DateTime.UtcNow;
 
     protected Device (string name, string location, DeviceType type)
     {
-        deviceName = name;
-        deviceLocation = location;
-        device = type;
+        DateTimeeviceName = name;
+        DeviceLocation = location;
+        Type = type;
     }
 
 //thought to keep this in because of how the api signatures look
@@ -26,6 +26,8 @@ public abstract class Device : IDevice
 //when the command pattern needs to run on specific devices
 //wanted to create a shared template for how devices handle command execution
 //each device will specify how this is done
+
+/*AO: Understood. :-) I assume we will remove it once we have the command pattern built out. */
 
     public void runCommands(DeviceCommand command){
         validateDeviceCommand(command); // validation should be universal across devices, help keep things DRY
