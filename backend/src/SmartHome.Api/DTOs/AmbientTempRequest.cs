0db55288f394fcf14a44730647d4664204ed1dde
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-04-18 20:57:08 -0400

    Scaffolding DTOs

---
+++ backend/src/SmartHome.Api/DTOs/AmbientTempRequest.cs
@@ -0,0 +1,9 @@
+namespace SmartHome.Api.DTOs;
+
+using SmartHome.Domain;
+
+public class AmbientTempRequest
+{
+    public int Temperature {get; set;}
+    //TODO: Create DTO for setting ambient temperature simulated by UI.
+}
--- backend/src/SmartHome.Domain/Interfaces/IDevice.cs
+++ backend/src/SmartHome.Domain/Interfaces/IDevice.cs
@@ -1,15 +1,16 @@
-   namespace SmartHome.Domain.Interfaces;
+namespace SmartHome.Domain.Interfaces;
 
-   public interface IDevice{
-   
-    public Guid Id {get;}
-    public String DeviceName {get;} 
-    public String DeviceLocation {get;} 
-    public DeviceType Type {get;}
-    //TODO: Create DeviceState class
-    //public DeviceState State {get;}
-    public Boolean IsDeviceOn {get;}
+public interface IDevice
+{
 
-    //TODO: Create DeviceSnapshot class
-    //public DeviceSnapshot dehydrate(); // we'll need this for persistence, reference section 2 in project doc
-   }
+   public Guid Id { get; }
+   public string DeviceName { get; }
+   public string DeviceLocation { get; }
+   public DeviceType Type { get; }
+   //TODO: Create DeviceState class
+   //public DeviceState State {get;}
+   public bool IsDeviceOn { get; }
+
+   //TODO: Create DeviceSnapshot class
+   //public DeviceSnapshot dehydrate(); // we'll need this for persistence, reference section 2 in project doc
+}
