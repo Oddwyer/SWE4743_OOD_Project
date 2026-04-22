ee919d2fe508c466ed5729984c637aa43817d098
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-04-22 19:22:15 -0400

    Skeleton for controllers in place.

---
+++ backend/src/SmartHome.Api/Controllers/SimulationController.cs
@@ -0,0 +1,13 @@
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHome.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SimulationController: ControllerBase
+{
+    
+}
+
+
