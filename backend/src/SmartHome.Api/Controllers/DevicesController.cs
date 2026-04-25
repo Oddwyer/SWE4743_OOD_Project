337f76720d33054f24e095cb29e691ecf718d697
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-04-25 18:58:30 -0400

    GET all devices, GET by id, DELETE created. WIP - POST

--- backend/src/SmartHome.Api/Controllers/DevicesController.cs
+++ backend/src/SmartHome.Api/Controllers/DevicesController.cs
@@ -3,12 +3,13 @@ using System.Linq;
 using SmartHome.Api.DTOs;
 using SmartHome.Api.Mappers;
 using SmartHome.Domain;
+using SmartHome.Domain.Interfaces;
 
 namespace SmartHome.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class DevicesController: ControllerBase
+public class DevicesController : ControllerBase
 {
     private readonly IDeviceService _deviceService;
 
@@ -17,26 +18,77 @@ public class DevicesController: ControllerBase
         _deviceService = deviceService;
     }
 
-    // Get endpoint to list all devices.
+    // GET: api/devices
     [HttpGet]
     // Return item of type iterable list of DeviceResponses, and whether successful.
     [ProducesResponseType(typeof(IEnumerable<DeviceResponse>), StatusCodes.Status200OK)]
-    public ActionResult<IEnumerable<DeviceResponse>> GetDevices()
+    public ActionResult<IEnumerable<DeviceResponse>> GetAllDevices()
     {
         var devices = _deviceService.GetAllDevices();
 
         // For each device in devices, translate using DeviceMapper and add to response.
         var response = devices.Select(DeviceMapper.ToResponse);
 
-    
+
         return Ok(response);
     }
 
-    //TODO:Methods for: 
-    //1. [HttpGet] -> single device
-    //2. [HttpPOST] -> register device
-    //3. [HttpPUT] -> update device
-    //4. [HttpDelete] -> delete device
+    // GET: api/devices/{id}
+    [HttpGet("{deviceId}")]
+    [ProducesResponseType(typeof(DeviceResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<DeviceResponse> GetDeviceById(Guid deviceId)
+    {
+
+       // Find and return device with matching ID.
+        var device = _deviceService.GetDeviceById(deviceId);
+
+        // If device could not be found, return status as not found.
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        // Return found device and successfull status.
+        var response = DeviceMapper.ToResponse(device);
+        return Ok(response);
+    }
+
+    // POST: api/devices/
+    [HttpPost("{device}")]
+    [ProducesResponseType(typeof(DeviceResponse), StatusCodes.Status200OK)]
+        public ActionResult<DeviceResponse> AddDevice(RegisterDeviceRequest request)
+    {
+        // Decompose request
+        var name = request.DeviceName;
+        var location = request.DeviceLocation;
+        var type = request.Type;
+
+        // 
+
+
+  
+    }
+    // DELETE: api/devices/{id}
+    [HttpDelete("{deviceId}")]
+    [ProducesResponseType(typeof(DeviceResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<DeviceResponse> RemoveDevice(Guid deviceId)
+    {
+
+       // Find and return device with matching ID.
+        var device = _deviceService.GetDeviceById(deviceId);
+
+        // If device could not be found, return status as not found.
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        // Return found device and successfull status.
+        _deviceService.RemoveDevice(device.Id);
+        return Ok();
+    }
 }
 
 
