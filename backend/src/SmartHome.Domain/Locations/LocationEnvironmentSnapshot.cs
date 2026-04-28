bb08540d4f3d56bfbecbd3ef5972c65f8f3b7ccf
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-04-27 21:30:55 -0400

    Added LocationEnvironmentSnapshot for persistence.

---
+++ backend/src/SmartHome.Domain/Locations/LocationEnvironmentSnapshot.cs
@@ -0,0 +1,7 @@
+namespace SmartHome.Domain.Locations;
+
+public class LocationEnvironmentSnapshot
+{
+    public string Location { get; set; } = "";
+    public int AmbientTemperature { get; set; }
+}
