9ed3d58ea834f742a908d6c175f6a6da053bf66d
Kataali N <kataalinashiru@gmail.com>
2026-05-02 13:42:06 -0400

    Finished the simulation ticker class and updated the ISimulationServices file with a few extra methods/functionalities

--- backend/src/SmartHome.Domain/Simulations/ISimulationService.cs
+++ backend/src/SmartHome.Domain/Simulations/ISimulationService.cs
@@ -1,3 +1,4 @@
+using SmartHome.Domain.Devices.Thermostat;
 namespace SmartHome.Domain.Simulations;
 
 /// <summary>
@@ -5,10 +6,21 @@ namespace SmartHome.Domain.Simulations;
 /// </summary>
 public interface ISimulationService
 {
+    // thermostat interaction methods/points
+    void RegisterThermostat(ThermostatDevice thermostat);
+    void UnregisterThermostat(ThermostatDevice thermostat);
+
+    // temperature control
     void SetAmbientTemperature(string location, int temperature);
     int GetAmbientTemperature(string location);
+    // once the ambient temperature is set, the simulation can use this method to update it without needing to set it again
+    void UpdateAmbientTemperature();
 
-    // TODO - Kataali: Implement full simulation behavior: tick timing, thermostat Heating/Cooling/Idle transitions, and reset rules.
+    // speed control
     void SetSimulationSpeed(SimulationSpeed speedMultiplier);
+    
+    // simulation control
+    void startSimulation();
     void ResetSimulation();
+    double elapsedSimulationSeconds();
 }
