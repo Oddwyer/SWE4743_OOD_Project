468eb42b6e382518f05c3d2e79515cbc567517c9
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-05-07 17:13:40 -0400

    WIP: Manage Devices Card (Remove/Add/History)

---
+++ frontend/smart-home-ui/src/app/components/manage-devices-card/manage-devices-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-devices-card',
+  imports: [],
+  templateUrl: './manage-devices-card.html',
+  styleUrl: './manage-devices-card.css',
+})
+export class ManageDevicesCard {}
