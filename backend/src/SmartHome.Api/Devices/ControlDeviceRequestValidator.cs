a783aba1537a389e1d792fa682523315b887ca47
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-05-07 23:37:37 -0400

    Added exclusivity clause in ControlDeviceRequestValidator.

--- backend/src/SmartHome.Api/Devices/ControlDeviceRequestValidator.cs
+++ backend/src/SmartHome.Api/Devices/ControlDeviceRequestValidator.cs
@@ -22,6 +22,8 @@ public class ControlDeviceRequestValidator : AbstractValidator<ControlDeviceRequ
         RuleFor(x => x.Brightness)
             .NotNull()
                 .WithMessage("Brightness is required.")
+            .ExclusiveBetween(LightDevice.MinBrightness, LightDevice.MaxBrightness)
+                .WithMessage($"Ambient temperature must be between {LightDevice.MinBrightness}% and {LightDevice.MaxBrightness}%.")
 
             .When(x => x.Command == DeviceCommandType.SetBrightness);
 
@@ -42,6 +44,8 @@ public class ControlDeviceRequestValidator : AbstractValidator<ControlDeviceRequ
         RuleFor(x => x.TargetTemperature)
             .NotNull()
                 .WithMessage("Desired temperature is required.")
+            .ExclusiveBetween(ThermostatDevice.MinTemperature, ThermostatDevice.MaxTemperature)
+                .WithMessage($"Ambient temperature must be between {ThermostatDevice.MinTemperature}°F and {ThermostatDevice.MaxTemperature}°F.")
             .When(x => x.Command == DeviceCommandType.SetTargetTemperature);
 
         RuleFor(x => x.ThermostatMode)
