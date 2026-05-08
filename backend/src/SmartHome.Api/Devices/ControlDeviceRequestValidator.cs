eb442a88c5183afb161db3319c40da6ebbf69946
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-05-07 23:39:43 -0400

    Typo error corrected in validator.

--- backend/src/SmartHome.Api/Devices/ControlDeviceRequestValidator.cs
+++ backend/src/SmartHome.Api/Devices/ControlDeviceRequestValidator.cs
@@ -23,7 +23,7 @@ public class ControlDeviceRequestValidator : AbstractValidator<ControlDeviceRequ
             .NotNull()
                 .WithMessage("Brightness is required.")
             .InclusiveBetween(LightDevice.MinBrightness, LightDevice.MaxBrightness)
-                .WithMessage($"Ambient temperature must be between {LightDevice.MinBrightness}% and {LightDevice.MaxBrightness}%.")
+                .WithMessage($"Brightness must be between {LightDevice.MinBrightness}% and {LightDevice.MaxBrightness}%.")
 
             .When(x => x.Command == DeviceCommandType.SetBrightness);
 
@@ -45,7 +45,7 @@ public class ControlDeviceRequestValidator : AbstractValidator<ControlDeviceRequ
             .NotNull()
                 .WithMessage("Desired temperature is required.")
             .InclusiveBetween(ThermostatDevice.MinTemperature, ThermostatDevice.MaxTemperature)
-                .WithMessage($"Ambient temperature must be between {ThermostatDevice.MinTemperature}°F and {ThermostatDevice.MaxTemperature}°F.")
+                .WithMessage($"Desired temperature must be between {ThermostatDevice.MinTemperature}°F and {ThermostatDevice.MaxTemperature}°F.")
             .When(x => x.Command == DeviceCommandType.SetTargetTemperature);
 
         RuleFor(x => x.ThermostatMode)
